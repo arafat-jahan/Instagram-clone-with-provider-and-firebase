ef18be593dd490acfca8586227485375abdc3a33
arafat-jahan <ajh.official15@gmail.com>
2026-01-31 22:03:46 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Instagram Clone',
+      title: 'Hello To Instagram',
+      color: Colors.grey,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor
       ),
